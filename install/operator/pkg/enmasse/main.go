d37318933c8e295a108ce1cc0ff1017478ed8045
Jameel Briones <jbriones@redhat.com>
2018-09-06 17:28:19 +0100

    unmarshal address space from configmap

--- install/operator/pkg/enmasse/main.go
+++ install/operator/pkg/enmasse/main.go
@@ -1,40 +1,56 @@
 package enmasse
 
 import (
-	"os"
+	"encoding/json"
 
 	glog "github.com/sirupsen/logrus"
 	"k8s.io/api/core/v1"
-
-	"github.com/operator-framework/operator-sdk/pkg/sdk"
-	"github.com/syndesisio/syndesis/install/operator/pkg/apis/syndesis/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/syndesisio/syndesis/install/operator/dep-cache/sources/https---github.com-operator--framework-operator--sdk/pkg/sdk"
+	enmasse "github.com/syndesisio/syndesis/install/operator/pkg/apis/enmasse/v1alpha1"
 )
 
 // Reconcile the state
-func Reconcile(configmap *v1.ConfigMap, deleted bool) error {
-	if val, ok := configmap.ObjectMeta.Labels["type"]; ok && val == "address-space-plan" {
-		glog.Infof("Reconciling address-space-plan: '%v'", configmap.ObjectMeta.Name)
-		return reconcileAddressSpacePlan(configmap, deleted)
+func ReconcileConfigmap(configmap *v1.ConfigMap, deleted bool) error {
+	// TODO: Get configmap of enmasse standard
+	if val, ok := configmap.ObjectMeta.Labels["type"]; ok && val == "address-space" {
+		return reconcileAddressSpace(configmap, deleted)
 	}
 	return nil
 }
 
-func reconcileAddressSpacePlan(addressSpacePlan *v1.ConfigMap, deleted bool) error {
-	asp := &v1alpha1.Connection{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Connection",
-			APIVersion: "syndesis.io/v1alpha1",
-		},
+func reconcileAddressSpace(configmap *v1.ConfigMap, deleted bool) error {
+	var addressSpace enmasse.AddressSpace
+	if err := json.Unmarshal([]byte(configmap.Data["config.json"]), &addressSpace); err != nil {
+		glog.Errorf("Failed to unmarshal the configmap data into an AddressSpace object %+v", err)
+		return nil
+	}
+
+	// TODO:Check if connection cr already exist
+
+	createKeycloakUser()
+	createConnectionCR()
+	return nil
+}
+
+func createKeycloakUser() error {
+	// TODO:Get keycloak credentials
+	keycloakCredSecret := &v1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "example",
-			Namespace: os.Getenv("WATCH_NAMESPACE"),
-		},
+
+		}
 	}
-	err := sdk.Get(asp)
-	if err != nil {
-		glog.Infof("getting asp gave error: %v", err.Error())
-		return err
-	}
+	sdk.Get()
+	// TODO:Get authenticated client + token
+	// TODO:Create user
+	return nil
+}
+
+func createConnectionCR() error {
+	// CR needs to have username, password and url
+	return nil
+}
+
+func getConnectionCR() error {
 	return nil
 }
